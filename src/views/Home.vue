928640fa6ec64b0de86fea6a2884af11973418f9
Simone Romiti <sr1992ei@gmail.com>
2021-04-09 16:13:52 +0200

    Add structure with vue-cli

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+   <h1>This is an home page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /sr
+
+
+export default {
+  name: "Home",
+  components: {
+    
+  },
+
+  data(){
+    return{
+      characters: []
+    }
+  },
+
+  mounted(){
+    this.axios
+      .get('https://www.breakingbadapi.com/api/characters?limit=10')
+      .then(response => {
+        console.log(response);
+      })
+  }
+};
+</script>
